663ecf788de521cb5f839ebf893573a74a7b5305
thedoritos <thedoritos@gmail.com>
2021-01-08 17:18:21 +0900

    :lipstick: Add min window size

--- Docca/ContentView.swift
+++ Docca/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TaskList()
+            .frame(minWidth: 320, minHeight: 480)
     }
 }
 
