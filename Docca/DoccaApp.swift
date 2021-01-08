302f840dd0eca1eebe1ec3d53d4a1f164baa8f30
thedoritos <thedoritos@gmail.com>
2021-01-08 14:54:33 +0900

    :tada: Create empty project

---
+++ Docca/DoccaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoccaApp.swift
+//  Docca
+//
+//  Created by Tomohiro Matsumura on 2021/01/08.
+//
+
+import SwiftUI
+
+@main
+struct DoccaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
